3c349e100bc37e564aa390e04b90d49bfc73ef9f
Chad Cunningham <chadcunningham7676@gmail.com>
2021-04-25 20:40:39 -0400

    Added update and create actions and made user_params method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(params.require(:user).permit(:name, :email))
+    user = User.new(user_params)
     if user.save
       render json: user
     else
@@ -15,5 +15,20 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     render json: user
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
