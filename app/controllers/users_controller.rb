8e2c8e4ccf9aa6d8b73e60c521bd6eb38cf977f6
Chad Cunningham <chadcunningham7676@gmail.com>
2021-04-25 20:32:15 -0400

    Added create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,10 @@ class UsersController < ApplicationController
   def show
     render json: params
   end
+
+  def create
+    user = User.new(params.require(:user).permit(:name, :email))
+    user.save!
+    render json: user
+  end
 end
