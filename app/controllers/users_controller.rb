bcfbeccc7f9538554f9bef73b704fd0817b95716
Chad Cunningham <chadcunningham7676@gmail.com>
2021-04-25 20:35:36 -0400

    Updated create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,16 +4,15 @@ class UsersController < ApplicationController
   end
 
   def create
-    render json: params
+    user = User.new(params.require(:user).permit(:name, :email))
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
   end
 
   def show
     render json: params
-  end
-
-  def create
-    user = User.new(params.require(:user).permit(:name, :email))
-    user.save!
-    render json: user
   end
 end
