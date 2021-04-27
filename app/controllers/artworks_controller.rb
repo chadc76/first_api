b3cc8d8460a26896d92391cc1d6c7e4a65cc6162
Chad Cunningham <chadcunningham7676@gmail.com>
2021-04-27 12:59:39 -0400

    Completed Artworks Controller and Phase II: Users and Artworks API

---
+++ app/controllers/artworks_controller.rb
@@ -0,0 +1,40 @@
+class ArtworksController < ApplicationController
+  def index
+    render json: Artwork.all
+  end
+  
+  def create
+    art = Artwork.new(artwork_params)
+    if art.save
+      render json: art
+    else
+      render json: art.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    art = Artwork.find(params[:id])
+    render json: art
+  end
+
+  def update
+    art = Artwork.find(params[:id])
+    if art.update(artwork_params)
+      render json: art
+    else
+      render json: art.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    art = Artwork.find(params[:id])
+    art.destroy
+    render json: art
+  end
+
+  private
+
+  def artwork_params
+    params.require(:artwork).permit(:title, :image_url, :artist_id)
+  end
+end
