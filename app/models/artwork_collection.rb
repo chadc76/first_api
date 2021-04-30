68b63f30e839964db98730a1d6d810ca647847ea
Chad Cunningham <chadcunningham7676@gmail.com>
2021-04-30 02:04:08 -0400

    Added access validation

--- app/models/artwork_collection.rb
+++ app/models/artwork_collection.rb
@@ -12,15 +12,28 @@ class ArtworkCollection < ApplicationRecord
   validates :collection_id, :artwork_id, presence: true
   validates :artwork_id, uniqueness: {scope: :collection_id,
     message: "has already been added to this collection!"}
+  validate :access
 
-    belongs_to :collection,
-      primary_key: :id,
-      foreign_key: :collection_id,
-      class_name: :Collection
+  belongs_to :collection,
+    primary_key: :id,
+    foreign_key: :collection_id,
+    class_name: :Collection
 
-    has_one :artwork,
-      primary_key: :artwork_id,
-      foreign_key: :id,
-      class_name: :Artwork
+  has_one :artwork,
+    primary_key: :artwork_id,
+    foreign_key: :id,
+    class_name: :Artwork
+  
+  def access
+    art = Artwork.find(artwork_id)
+    user_id = Collection.find(collection_id).owner.id
+    if (art.artist_id == user_id)
+      return
+    elsif (art.shared_viewers.map(&:id).include?(user_id))
+      return
+    else
+      errors.add(:user_id, "doesn't have permission to add this artwork to their collection!")
+    end
+  end
   
 end
