6543045c4edb6a604fbb2f5b2500ee768e136d8e
Chad Cunningham <chadcunningham7676@gmail.com>
2021-04-30 02:10:21 -0400

    Added new routes for collections

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :create, :show, :update, :destroy] do
     resources :artworks, only: [:index]
+    resources :collection, only: [:index]
   end
   resources :artworks, only: [:create, :show, :update, :destroy] do
     member do
@@ -12,17 +13,28 @@ Rails.application.routes.draw do
       post 'unfavorite'
     end
   end
+
   resources :artwork_shares, only: [:create, :destroy] do 
     member do
       post 'favorite'
       post 'unfavorite'
     end
   end
+
   resources :comments, only: [:index, :create, :destroy] do
     member do
       get 'likes'
       post 'like'
       post 'unlike'
+    end
+  end
+
+  resources :collections, only: [:create, :show, :destroy] do
+    resources :artworks, only: [:index] do
+      member do
+        post :add, to: 'collections#add_artwork', as: 'add'
+        post :remove, to: 'collections#remove_artwork', as: 'remove'
+      end
     end
   end
 end
