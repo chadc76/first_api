94c3c46b1a499939dc10a5f04434e1baf2b4e61f
Chad Cunningham <chadcunningham7676@gmail.com>
2021-04-30 15:19:59 -0400

    Updated Seed file to populate collections

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :create, :show, :update, :destroy] do
     resources :artworks, only: [:index]
-    resources :collection, only: [:index]
+    resources :collections, only: [:index]
   end
   resources :artworks, only: [:create, :show, :update, :destroy] do
     member do
